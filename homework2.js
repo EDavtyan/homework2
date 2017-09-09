781a2868cda99550fffbe45509cb03bb7293013e
Edgar Davtyan <edo99davtyan@gmail.com>
2017-09-09 20:56:15 +0400

    Uploading the homework number 2.

---
+++ homework2.js
@@ -0,0 +1,13 @@
+alert("Click On Pictures! :)");
+
+const Fruits = "banans, apples, peaches, ";
+const Veggies = "eggplants, broccoli, spinach ";
+const BUFF = "Better Understanding of Food and Fitness ";
+
+const HealthyLife = BUFF + "is when you eat " + Fruits + Veggies + "and do not forget about carbs and proteins.";
+console.log(HealthyLife);
+
+//different from the main topic
+const temperature = 36.6;
+AvgHumanBodyTempInFahrenheit = (temperature + 9/5 + 32);
+console.log (AvgHumanBodyTempInFahrenheit);
